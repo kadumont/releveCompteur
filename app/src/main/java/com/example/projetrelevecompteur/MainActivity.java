8831fa1ee2b62b9d47eb90b8efd110e5f5969c52
JeremM <jeremynght@yahoo.com>
2021-11-18 15:29:01 +0100

    Changement de page

--- app/src/main/java/com/example/projetrelevecompteur/MainActivity.java
+++ app/src/main/java/com/example/projetrelevecompteur/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.projetrelevecompteur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Btn Connexion
+    public void envoyerConnexion (View view) {
+        Intent intent = new Intent(this,ConnectionFinal.class);
+
+        startActivity(intent);
+    }
+
+    //Btn ListeCompteur
+    public void envoyerListeCompteur (View view) {
+        Intent intent = new Intent(this,ControleurListeCompteur.class);
+
+        startActivity(intent);
+    }
+
+    //Btn FicheCompteur
+    public void envoyerFicheCompteur (View view) {
+        Intent intent = new Intent(this,ControleurFicheCompteur.class);
+
+        startActivity(intent);
     }
 }
