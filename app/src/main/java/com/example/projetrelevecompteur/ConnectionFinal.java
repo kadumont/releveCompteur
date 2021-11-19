a18a4073211b402ba23d2a578dca9d91f17fc835
Lary <kensy.lanw@gmail.com>
2021-11-19 16:40:01 +0100

    rajout de la librairie + Commencement du ComboDialogue

--- app/src/main/java/com/example/projetrelevecompteur/ConnectionFinal.java
+++ app/src/main/java/com/example/projetrelevecompteur/ConnectionFinal.java
@@ -3,6 +3,9 @@ package com.example.projetrelevecompteur;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import plum.widget.ComboDialog;
 
 public class ConnectionFinal extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class ConnectionFinal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection_final);
+    }
+
+    TextView myTextViewItem = (TextView)findViewById( R.id.idUtilisateur );
+
+    final CharSequence[] items = {"Rouge","Vert","Bleu"};
+    final CharSequence[] values = {"1","2","3"};
+
+    ComboDialog comboCouleur = new ComboDialog( "Choisir une couleur",
+            items,
+            values,
+            myTextViewItem,
+            this );
+
+    //Mise en place d'un écouteur sur ComboDialog
+        comboCouleur.setOnClickComboDialogListener(this);
+
+
+
+    // Le choix dans ComboDialog déclenche un clic
+    public void onClickComboDialog( ComboDialog comboDialog )
+    {
+        String value = (String) comboDialog.value( comboDialog.getIndexSelected());
+        String item = (String) comboDialog.item( comboDialog.getIndexSelected());
+
+        Toast toast = Toast.makeText(getApplicationContext(),
+                value + ":" + item,
+                Toast.LENGTH_LONG);
+        toast.show();
     }
 }
