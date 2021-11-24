3e55fe07282f2038b3c46a63fc5db8d346d0ec2a
Lary <kensy.lanw@gmail.com>
2021-11-24 14:17:25 +0100

    Identifiant ComboDialog Ok

--- app/src/main/java/com/example/projetrelevecompteur/ConnectionFinal.java
+++ app/src/main/java/com/example/projetrelevecompteur/ConnectionFinal.java
@@ -9,35 +9,41 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import plum.widget.ComboDialog;
 
-public abstract class ConnectionFinal extends AppCompatActivity implements ComboDialog.OnClickComboDialogListener{
+public class ConnectionFinal extends AppCompatActivity implements ComboDialog.OnClickComboDialogListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection_final);
+
+        CompteurSQLLite sqlLite = new CompteurSQLLite(this);
+        TextView myTextViewItem = (TextView)findViewById( R.id.idUtilisateur );
+
+
+        ArrayList<Releveur> listeReleveur = sqlLite.getListeReleveur();
+
+        final CharSequence[] itemsId  = new CharSequence[listeReleveur.size()];
+        final CharSequence[] valuesMotDePasse = new CharSequence[listeReleveur.size()];
+
+        for(int i = 0; i < listeReleveur.size() ; i++ ){
+            itemsId[i] = listeReleveur.get(i).nomReleveur;
+            valuesMotDePasse[i] = listeReleveur.get(i).motDePasse;
+        }
+
+
+        ComboDialog comboCouleur = new ComboDialog( "Choisir un identifiant",
+                itemsId,
+                valuesMotDePasse,
+                myTextViewItem,
+                this );
+
+        //Mise en place d'un écouteur sur ComboDialog
+        comboCouleur.setOnClickComboDialogListener(this);
+
     }
 
-    TextView myTextViewItem = (TextView)findViewById( R.id.idUtilisateur );
-
-    ArrayList<Releveur> listeReleveur = new ArrayList();
 
 
-    final CharSequence[] itemsId = {"Rouge","Vert","Bleu"}; //identifiant
-    final CharSequence[] valuesMotDePasse = {"1","2","3"}; //mot de passe
-
-    for(int i = 0, listeReleveurw ){
-
-    }
-
-
-    ComboDialog comboCouleur = new ComboDialog( "Choisir un identifiant",
-            itemsId,
-            itemsId,
-            myTextViewItem,
-            this );
-
-    //Mise en place d'un écouteur sur ComboDialog
-    comboCouleur.setOnClickComboDialogListener(this);
 
 
 
