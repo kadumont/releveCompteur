7590cc493bf2f68e70fd27987e64b51e0d9cb2cd
Charletoux Théoxane <theoxane.charletoux@educ-valadon-limoges.fr>
2021-11-25 14:06:23 +0100

    ya qql chose

--- app/src/main/java/com/example/projetrelevecompteur/ControleurFicheCompteur.java
+++ app/src/main/java/com/example/projetrelevecompteur/ControleurFicheCompteur.java
@@ -3,6 +3,7 @@ package com.example.projetrelevecompteur;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ControleurFicheCompteur extends AppCompatActivity {
 
@@ -11,9 +12,29 @@ public class ControleurFicheCompteur extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controleur_fiche_compteur);
 
-        CompteurSQLLite cbd = new CompteurSQLLite;
+        CompteurSQLLite cbd = new CompteurSQLLite(this);
         Compteur c = cbd.getListeCompteur().get(0);
 
+        TextView txtNom=(TextView)findViewById(R.id.txtNom);
+        txtNom.setText(c.nom);
+
+        TextView txtRue=(TextView)findViewById(R.id.txtRue);
+        txtRue.setText(c.rue);
+
+        TextView txtCodePostal=(TextView)findViewById(R.id.txtCodePostal);
+        txtCodePostal.setText(c.codePostal);
+
+        TextView txtVille=(TextView)findViewById(R.id.txtVille);
+        txtVille.setText(c.ville);
+
+        TextView txtAncienIndex=(TextView)findViewById(R.id.txtIndexAncien);
+        txtAncienIndex.setText(c.indexAncien + "");
+
+        TextView txtIndexNouveau=(TextView)findViewById(R.id.txtIndexNouveau);
+        txtIndexNouveau.setText(c.indexNouveau + "");
+
+        TextView txtNomReleveur=(TextView)findViewById(R.id.txtNomReleveur);
+        txtNomReleveur.setText(c.nomReleveur);
 
     }
 }
